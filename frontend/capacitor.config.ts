e31c2448e2dff2294c448960836c173c4971cd02
Good-Loops <michel.sdf@gmail.com>
2026-08-17 05:01:06 -0300

    fix(deps): upgrade frontend and Capacitor

--- frontend/capacitor.config.ts
+++ frontend/capacitor.config.ts
@@ -3,7 +3,7 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
     appId: 'org.mickeyf.app',
     appName: 'BeatCalc',
-    webDir: './public',
+    webDir: './dist',
 };
 
 export default config;
